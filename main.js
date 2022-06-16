1d86cdd78c26ee8b4e6404c5a72215aa85ed4b6e
Elliott Tay <elliotttay@Elliotts-MacBook-Pro.local>
2022-06-16 16:09:46 +0800

    Added in some playing functions and improve on the display

--- main.js
+++ main.js
@@ -1,119 +1,114 @@
-//Create variables for storage later (like computer sequence and human sequence). No const as these variables need to be amended
-let computerOrder = [];
+//initialize computer and human sequence to compare later
+let computer = [];
 let player = [];
-let flash;
-let turn;
-let good;
-let computerTurn;
-let interval;
-let noise = true;
-let on = false;
-let win;
-let gameover;
-let levels = 30;
 let life = 5;
+let levels = 0;
 
-//Select all the buttons from the html file for us to use later and put it as constants
-const startButton = document.querySelector("#start")
-const yellowButton = document.querySelector('#yellow-button');
-const blueButton = document.querySelector('#blue-button');
-const greenButton = document.querySelector('#green-button');
-const redButton = document.querySelector('#red-button');
+//start game function
+startButton = document.querySelector('#start');
 
-//Attach event listener to the start button
+//when button is clicked on, game starts and runs
 startButton.addEventListener('click', (event) => {
-    if (win) {
-        play();
-    }
-});
+    //start game function
+    console.log('Hello there');
+})
 
-function play() {
-    win = false;
-    computerOrder = [];
-    player = [];
-    flash = 0;
-    interval = 0;
-    turn = 1;
-    good = true;
-    if (life !== 0) {
-        for (let i = 0; i < levels; i++) {
-            computerOrder.push(Math.floor(Math.random() * 4) + 1);
-        }
-    }
-    computerTurn = true;
-    interval = setInterval(Turn, 1000);
+//Initializing buttons for play
+yellowButton = document.querySelector('#yellow-button');
+blueButton = document.querySelector('#blue-button');
+greenButton = document.querySelector('#green-button');
+redButton = document.querySelector('#red-button');
 
-    if (life === 0) {
-        gameOver();
+//Display Levels
+displayLevel = document.querySelector('#display-level');
+displayLevel.innerText = levels;
+
+//Display Life
+displayLife = document.querySelector('#life');
+displayLife.innerHTML = 'Life: ' + life;
+
+//flash colour functions to use when in need of flashing colours
+function flashColour() {
+    yellowButton.style.background = 'lightyellow';
+    blueButton.style.background = 'darkblue';
+    greenButton.style.background = 'lightgreen';
+    redButton.style.background = 'pink';
+}
+
+//Sets the colours back to normal after flashing
+function clearColour() {
+    yellowButton.style.background = 'rgb(204, 204, 0)';
+    blueButton.style.background = 'skyblue';
+    greenButton.style.background = 'rgb(0, 128, 0)';
+    redButton.style.background = 'rgb(227, 0, 34)';
+}
+
+//Randomizes array for up to 30 levels
+function random() {
+    const array = ['yellowButton', 'blueButton', 'greenButton', 'redButton'];
+    for(let i = 0; i < levels; i++) {
+        computer.push(Math.floor(Math.random() * 4) + 1);
     }
 }
 
-function Turn() {
-    on = false;
-    
-    if(flash == turn) {
-        clearInterval(interval);
-        computerTurn = false;
-        clearColour();
-        on = true;
-    }
-
-    if (computerTurn) {
-        clearColour();
-        setTimeout(() => {
-            if (computerOrder[flash] == 1) {
-                yellow();
-            }
-
-            if (computerOrder[flash] == 2) {
-                blue();
-            }
-
-            if (computerOrder[flash]== 3) {
-                green();
-            }
-
-            if (computerOrder[flash]== 4) {
-                red();
-            }
-            flash++;
-        }, 300);
-    }
-}
-
-//Functions to use when the button is clicked on
+//Yellow button onclick, record it and push it into the player array
 function yellow() {
-    if (noise) {
-        let audio = document.getElementById("clip1");
-        audio.play;
-    }
-    noise = true;
-    yellowButton.getElementsByClassName.backgroundColor = "lightyellow";
+    yellowButton.addEventListener('click', (event) => {
+        player.push(1);
+    }) 
 }
 
+//Blue button onclick, record it and push it into the player array
 function blue() {
-    if (noise) {
-        let audio = document.getElementById("clip2");
-        audio.play;
-    }
-    noise = true;
-    blueButton.getElementsByClassName.backgroundColor = "darkblue";
+    blueButton.addEventListener('click', (event) => {
+        player.push(2);
+    }) 
 }
 
+//Green button onclick, record it and push it into the player array
 function green() {
-    if (noise) {
-        let audio = document.getElementById("clip3");
-        audio.play;
-    }
-    noise = true;
-    greenButton.getElementsByClassName.backgroundColor = "lightgreen";
+    greenButton.addEventListener('click', (event) => {
+        player.push(3);
+    }) 
 }
 
+//Red button onclick, record it and push it into the player array
 function red() {
-    if (noise) {
-        let audio = document.getElementById("clip4");
-        audio.play();
-    }
-    noise = true;
-    redButton.getElementsByClassName.backgroundColor = "lightred";
+    redButton.addEventListener('click', (event) => {
+        player.push(4);
+    })
 }
+
+//Win function to alert the user that they have already won
+function win() {
+    const winning = document.querySelector('#win');
+    console.log(winning);
+    if (levels === 30) {
+        winning.innerText = "Congratulations you have won the game!";
+        //insert reset function here
+    } else {
+        winning.innerText = "Game is in session";
+    }
+}
+
+//Gameover function to end the game when the game is over
+function gameOver() {
+    if (life === 0) {
+        alert("Gameover!")
+        //insert reset function
+    } else {
+        return
+    }
+}
+
+//Check function to see if both arrays are the same
+
+//Reset game function to set everything back to its initial state
+
+//Computer turn function
+
+//Human turn function
+
+//Play function
+
+
